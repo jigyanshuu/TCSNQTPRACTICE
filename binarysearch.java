f665d8f113771f380cdc9ec63e99f3a65f953916
Jigyanshu Sabata <164847642+jigyanshuu@users.noreply.github.com>
2026-05-30 20:00:50 +0530

    Add files via upload

--- binarysearch.java
+++ binarysearch.java
@@ -1,39 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class binarysearch {
-        public static int solve(int[] arr, int k)
-        {
-                int n=arr.length;
-                int low=0;
-                int high=n-1;
-                int ans=-1;
-                while(low<=high)
-                {
-                        int mid=low + (high-low) / 2;
-                        if(arr[mid] == k)
-                        {
-                                ans=mid;
-                                break;
-                        }
-                        else if(arr[mid] <k)
-                        {
-                                low=mid+1;
-                        }
-                        else{
-                                high=mid-1;
-                        }
-                }
-                return ans;
+    public static boolean search(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == k) {
+                return true; 
+            } else if (arr[mid] < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        public static void main(String[] args) {
-                Scanner sc=new Scanner(System.in);
-                int n=sc.nextInt();
-                int[] arr=new int[n];
-                int k=sc.nextInt();
-                for(int i=0;i<n;i++)
-                {
-                        arr[i]=sc.nextInt();
-                }
-                System.out.println(solve(arr, k));
+        return false; 
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-}
+        Arrays.sort(arr);        
+        if (search(arr, k)) {
+            System.out.println(k + " found out"); 
+        } else {
+            System.out.println(-1);
+        }
+        sc.close();
+    }
+} 
